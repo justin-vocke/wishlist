92a587dbe6a136ff5046aaf7438188805611dc2d
Justin Vocke <therealmathhelper@gmail.com>
2022-03-19 22:37:46 -0400

    first commit

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var allItems = _context.Items.ToList();
+            
+            return View(allItems);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item) 
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpDelete]
+        public IActionResult Delete(int Id)
+        {
+            var itemToDelete = _context.Items.Where(x => x.Id == Id).FirstOrDefault();
+
+            _context.Items.Remove(itemToDelete);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
